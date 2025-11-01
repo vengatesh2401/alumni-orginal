9e1b6363a5360c9f0b7564dfecc65e41e67fba6f
aravinth0606 <senthilaravinth8@gmail.com>
2025-11-01 20:58:18 +0530

    12th commit message from aravinth

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Router } from 'react-router-dom'
+import Home from './pages/Home.jsx'
 
 function App() {
   
-
   return (
-   <BrowserRouter>
-    <Routes>
-
-    </Routes>
-   </BrowserRouter>  )
+  <>
+  <div className="app">
+    <Home />
+  </div>
+  </>
+  )
 }
 
 export default App
