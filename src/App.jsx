375f9294f3736fc4883d2ae6a5a3bd652c7107c5
aravinth0606 <senthilaravinth8@gmail.com>
2025-11-01 18:54:27 +0530

    seventh commit message from vengatesh

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Home from './pages/Home.jsx'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-
-      </Routes>
-    </BrowserRouter>
+    <>
+    </>
   )
 }
 
