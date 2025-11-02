a5c6c9877a436f9e7789d606f42ad9da01b3705f
aravinth0606 <senthilaravinth8@gmail.com>
2025-11-02 13:15:39 +0530

    deleted some file from vengatesh

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import { BrowserRouter,Routes,Route } from "react-router-dom"
-import Home from './pages/Home.jsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 const App = () => {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
